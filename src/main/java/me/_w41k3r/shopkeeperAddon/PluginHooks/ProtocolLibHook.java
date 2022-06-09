50caa19dc75fe359dc4e5310413e515a81274fed
the-w41k3r <64218536+the-w41k3r@users.noreply.github.com>
2022-06-09 22:38:06 +0530

    Revert Back

---
+++ src/main/java/me/_w41k3r/shopkeeperAddon/PluginHooks/ProtocolLibHook.java
@@ -0,0 +1,4 @@
+package me._w41k3r.shopkeeperAddon.PluginHooks;
+
+public class ProtocolLibHook {
+}
---
+++ src/main/java/me/_w41k3r/shopkeeperAddon/TabComplete.java
@@ -0,0 +1,40 @@
+package me._w41k3r.shopkeeperAddon;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+public class TabComplete implements TabCompleter {
+    public List<String> library = new ArrayList<>();
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if(library.isEmpty())
+        {
+            library.add("help");
+            library.add("shops");
+            library.add("playershops");
+            library.add("shop");
+            library.add("reload");
+        }
+        List<String> result = new ArrayList<String>();
+        if(args.length == 1)
+        {
+            for (String a : library)
+            {
+                if(a.toLowerCase().startsWith(args[0].toLowerCase()))
+                {
+                    result.add(a);
+                }
+            }
+            return result;
+        }
+        return null;
+    }
+        
+
+}
